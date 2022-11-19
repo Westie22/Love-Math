a4ae04c59ca732381d55e4bb2fe936fa4cd34905
Westie22 <jinyoungwest@gmail.com>
2022-11-19 14:49:30 +0000

    Set buttons fuctions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// wait for the DO< to finish loading before running the game
+// Get the button elements and add event linstenrs to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+                
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -27,5 +46,5 @@ function displaySubtractQuestin() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
